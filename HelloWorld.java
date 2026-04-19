b98b4d0bad402da982274db2a2b042a230b5050b
Dewprakash Chandra <dewchandra20@gmail.com>
2026-04-19 10:00:48 +0530

    Add HelloWorld.java with multiple System.out.println statements and remove TestFile.txt

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It is used for demonstration purposes.");
+        System.out.println("Adding some more SOP statements as requested.");
+        System.out.println("Have a great day!");
+    }
+}
